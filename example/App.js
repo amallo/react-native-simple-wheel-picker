a624d1e21c888c18cbfc591766208f6f50f83f66
Mallogia Audie <audie@app2b.io>
2020-01-08 18:52:57 +0100

    feature: add top / bottom space items

--- example/App.js
+++ example/App.js
@@ -6,74 +6,109 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {ScrollView, Text, View, StyleSheet} from 'react-native';
-import {WheelPicker} from 'react-native-simple-wheel-picker';
+import React, { Component } from 'react';
+import { ScrollView, Text, View, StyleSheet } from 'react-native';
 
 const itemSize = 40;
+const countItem = 10;
+const visibleItem = 10;
+const marginHorizontal = 40
+const visibleHeight = visibleItem * itemSize;
+const separatorSize = 5
+
+const DATA = [...Array(countItem).keys()].map((index) => `P${index}`)
+
 const styles = StyleSheet.create({
   item: {
+    width: '100%',
     height: itemSize,
     borderWidth: 1,
     borderColor: 'green',
-    textAlign: 'center',
+    textAlign: 'center'
   },
   upperSeparator: {
     position: 'absolute',
-    height: 5,
+    height: separatorSize,
     backgroundColor: 'purple',
-    top: itemSize * 5,
-    left: itemSize,
-    right: itemSize,
+    left: marginHorizontal,
+    right: marginHorizontal,
   },
   lowerSeparator: {
     position: 'absolute',
-    height: 5,
+    height: separatorSize,
     backgroundColor: 'purple',
-    left: 40,
-    right: 40,
-    top: itemSize * 5 + itemSize,
+    left: marginHorizontal,
+    right: marginHorizontal,
   },
 });
 
+const getSeparatorPosition = (visibleItem, visibleHeight, itemSize) => {
+  if (visibleItem % 2 === 0) {
+    const upperSeparatorPosition = (visibleHeight / 2)
+    const lowerSeparatorPosition = upperSeparatorPosition + itemSize
+    return {
+      upperSeparatorPosition,
+      lowerSeparatorPosition
+    }
+  }
+  else {
+    const lowerSeparatorPosition = ((visibleHeight + itemSize) / 2)
+    const upperSeparatorPosition = lowerSeparatorPosition - itemSize
+    return {
+      upperSeparatorPosition,
+      lowerSeparatorPosition
+    }
+  }
+}
+
 class App extends Component {
   render() {
+    const pos = getSeparatorPosition(visibleItem, visibleHeight, itemSize)
+    const separatorStartIndex = Math.trunc(pos.upperSeparatorPosition / itemSize)
+    const separatorEndIndex = separatorStartIndex + 1
+    const topSpaceItem = visibleItem - separatorEndIndex
+    const bottomSpaceItem = separatorStartIndex
+    console.log('visible', DATA.slice(0, visibleItem))
+    console.log('hidden', DATA.slice(visibleItem))
     return (
-      <View style={{height: 400}}>
-        <View style={styles.upperSeparator} />
-        <View style={styles.lowerSeparator} />
-        <ScrollView
-          decelerationRate={0.999}
-          snapToInterval={itemSize}
-          contentContainerStyle={{
-            borderColor: 'red',
-            borderWidth: 2,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-          <View style={styles.item} />
-          <View style={styles.item} />
-          <View style={styles.item} />
-          <View style={styles.item} />
-          <View style={styles.item} />
+      <View style={{ flexDirection: 'column', justifyContent: 'flex-end', flex: 1 }}>
+        <View style={{ height: visibleHeight }}>
+          <View style={[styles.upperSeparator, { top: pos.upperSeparatorPosition }]} />
+          <View style={[styles.lowerSeparator, { top: pos.lowerSeparatorPosition }]} />
+          <ScrollView
+            decelerationRate={0.999}
+            snapToInterval={itemSize}
+            contentContainerStyle={{
+              borderColor: 'red',
+              borderWidth: 2,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            {
+              [...Array(bottomSpaceItem).keys()].map((index) => {
+                return <View key={`bottom${index}`} style={styles.item} />
+              })
+            }
 
-          <Text style={styles.item}>Choose</Text>
-          <Text style={styles.item}>Portugal</Text>
-          <Text style={styles.item}>England</Text>
-          <Text style={styles.item}>Portugal</Text>
-          <Text style={styles.item}>England</Text>
-          <Text style={styles.item}>Portugal</Text>
-          <Text style={styles.item}>England</Text>
-          <Text style={styles.item}>Portugal</Text>
-          <Text style={styles.item}>England</Text>
-          <Text style={styles.item}>England</Text>
+            {
+              DATA.slice(0, visibleItem).map((value, index) => {
+                return <Text key={`visible${index}`} style={styles.item}>{value}</Text>
+              })
+            }
+            {
+              DATA.slice(visibleItem).map((value, index) => {
+                return <Text key={`hidden${index}`} style={styles.item}>{value}</Text>
+              })
+            }
+            {
+              [...Array(topSpaceItem).keys()].map((index) => {
+                return <View key={`top${index}`} style={styles.item} />
+              })
+            }
 
-          <View style={styles.item} />
-          <View style={styles.item} />
-          <View style={styles.item} />
-          <View style={styles.item} />
-        </ScrollView>
-      </View>
+          </ScrollView>
+        </View>
+      </View >
     );
   }
 }
